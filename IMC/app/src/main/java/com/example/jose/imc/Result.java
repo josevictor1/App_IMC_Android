2d8fd953d2c5414c8a0e4e297148bf2e62fe34b2
josevictor1 <josevictorpatos@gmail.com>
2017-10-12 23:21:55 -0300

    com imagem

--- IMC/app/src/main/java/com/example/jose/imc/Result.java
+++ IMC/app/src/main/java/com/example/jose/imc/Result.java
@@ -3,6 +3,7 @@ package com.example.jose.imc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.graphics.Color;
@@ -17,24 +18,29 @@ public class Result extends AppCompatActivity {
 
 
         TextView textView = (TextView) findViewById(R.id.textResult);
-
+        ImageView mImageView = (ImageView) findViewById(R.id.imageDisplay);
         double imc = Double.parseDouble(getIntent().getStringExtra("result"));
 
         if (imc < 18.5) {
 
             textView.setText("Abaixo do peso");
+            mImageView.setImageResource(R.drawable.abaixopeso);
+
         }
         else if (imc < 25) {
 
             textView.setText("Saudável");
+            mImageView.setImageResource(R.drawable.normal);
         }
         else if (imc < 30) {
 
             textView.setText("Acima do peso");
+            mImageView.setImageResource(R.drawable.acimapeso);
         }
         else {
 
             textView.setText("Obesidade");
+            mImageView.setImageResource(R.drawable.obeso);
         }
 
     }
